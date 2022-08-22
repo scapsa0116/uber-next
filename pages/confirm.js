15ebef859eb0d8deebf3508c8ae23ba621d0aaa0
stela <stela.capsa@gmail.com>
2022-08-22 21:03:13 +0100

    json data

--- pages/confirm.js
+++ pages/confirm.js
@@ -6,24 +6,41 @@ import Map from './components/map'
 
 const Confirm = () => {
 
-  const getCoordinations = () => {
+  const getPickupCoordinates = () => {
     const location = "Santa Monica"
 
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` + 
      new URLSearchParams({ 
-      access_token: "pk.eyJ1Ijoic2NhcHNhMDExNiIsImEiOiJjbDZkdDh1eHIwMWE0M2lydzJqdW1kaWNqIn0.eXwFPx-HHljcYye_Pd9JUA"
+      access_token: "pk.eyJ1Ijoic2NhcHNhMDExNiIsImEiOiJjbDZkdDh1eHIwMWE0M2lydzJqdW1kaWNqIn0.eXwFPx-HHljcYye_Pd9JUA",
+      limit: 1
      }) 
      )
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log(data.features[0].center)
+    })
+  }
+
+  const getDropoffCoordinates = () => {
+    const location = "Santa Monica"
+
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` + 
+     new URLSearchParams({ 
+      access_token: "pk.eyJ1Ijoic2NhcHNhMDExNiIsImEiOiJjbDZkdDh1eHIwMWE0M2lydzJqdW1kaWNqIn0.eXwFPx-HHljcYye_Pd9JUA",
+      limit: 1
+     }) 
+     )
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.features[0].center)
     })
   }
 
 
-  // useEffect(()=>{
-  //   getCoordinations();
-  // }, [])
+  useEffect(()=>{
+    getPickupCoordinates();
+    getDropoffCoordinates()
+  }, [])
 
 
 
